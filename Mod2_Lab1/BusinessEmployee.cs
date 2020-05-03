1a8b824d17102ccefd98776c7012cc59ee9bee1c
nouseph <61069607+nouseph@users.noreply.github.com>
2020-05-02 18:41:52 -0700

    Add files via upload

---
+++ Mod2_Lab1/BusinessEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod2_Lab1
+{
+    //BusinessEmployee class inheriting from Employee
+    class BusinessEmployee:Employee
+    {
+        //calls upon base class constructor from within derived class 
+        //sets the basesalary to 50000 for all businessemployee objects
+
+        public BusinessEmployee(string name) : base(name, 50000)
+        {
+
+        }
+        //creates double variable called bonusbudget and assigns value to 1000
+        public double bonusBudget = 1000;
+
+        //this method returns toString() method which is employee's id and name and prints the bonus budget
+
+
+        public override string employeeStatus()
+        {
+            return toString() + " with a budget of " + bonusBudget+".";
+
+        }
+    }
+}
---
+++ Mod2_Lab1/Employee.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod2_Lab1
+{
+     class Employee
+    {
+        //3 private memeber variables:employeeNme,employeeBaseSalary and employeeId
+        private string employeeName;
+        private double employeeBaseSalary;
+        private int employeeId;
+
+        //public properties
+
+        public string Name
+        {
+            get 
+            { 
+                return employeeName;
+            }
+            set { 
+                employeeName = value;
+            }
+
+        }
+        public double BaseSalary
+        {
+            get
+            {
+                return employeeBaseSalary;
+
+            }
+            set
+            {
+                employeeBaseSalary = value;
+
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return employeeId;
+            }
+            set
+            {
+                employeeId = value;
+
+            }
+        }
+        //Creating a constructor
+        public Employee(string name,double baseSalary)
+        {
+            Name = name;
+            BaseSalary = baseSalary;
+            ID = employeeCount++;
+
+
+        }
+        //method to return employees base salary
+
+        public double getBaseSalary()
+        {
+            return BaseSalary;
+
+        }
+        //method to return employee name
+
+        public string getName()
+        {
+            return Name;
+
+        }
+        //creates integer variable called employeeCount and assigns value to 1
+
+        private static int employeeCount = 1;
+
+        //method to return employee id
+        public int getEmployeeID()
+        {
+            return ID;
+
+        }
+        //method returns the employee's id and name
+        public string toString()
+        {
+            return ID + " " + Name;
+
+        }
+        //this method returns the employee's Id and name and confirms that the employee is in the system
+
+
+
+        public virtual string employeeStatus()
+        {
+            return toString() + " is in the companys system";
+
+        }
+        
+    }
+}
